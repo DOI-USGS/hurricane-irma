e1e017e4ead6154534a92433c945de136c3d82db
Jordan S Read <jread@usgs.gov>
2017-09-07 21:57:54 -0500

    write and read svg map

--- scripts/visualize/visualize.hurricane_map.R
+++ scripts/visualize/visualize.hurricane_map.R
@@ -1,5 +1,8 @@
 #' function for placing add-ons to the svg base map
 #' 
 visualize.hurricane_map <- function(viz = as.viz('hurricane-map')){
-  # read in the file, add to it, write it again
+  library(xml2)
+  svg <- readDepends(viz)[[1]]
+  
+  write_xml(svg, file = viz[['location']])
 }
