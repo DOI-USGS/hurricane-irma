88f779f8c70641498976c4ec956b30d8d3f5df9b
wdwatkins <wwatkins@usgs.gov>
2017-09-08 17:28:22 -0500

    sites working

--- scripts/fetch/fetch.sites.R
+++ scripts/fetch/fetch.sites.R
@@ -10,13 +10,18 @@ fetch.sites <- function(viz = as.viz('sites-data')){
   start.date <-  as.Date(getContentInfo('precip-data')[['start.date']])
   
   site_sum_all <- data.frame()
-  
+  counties_fips <- maps::county.fips %>% 
+    dplyr::filter(polyname %in% names(counties)) %>% 
+    mutate(fips = sprintf('%05d', fips))
   #max 20 counties at once
-  for(i in ){
+  reqBks <- seq(1,nrow(counties_fips),by=20)
+  
+  for(brk in reqBks){
+    fips_subset <- na.omit(counties_fips$fips[brk:(brk+19)])
     sites <- dataRetrieval::readNWISdata(service = "site",
                           seriesCatalogOutput=TRUE,
                           parameterCd=pCode,
-                          stateCd = i)
+                          countyCd = fips_subset)
     sites_sum <- filter(sites, parm_cd == pCode,
                         data_type_cd == "uv",
                         !site_tp_cd %in% c("LK", "ES", "GW")) %>% #others?
