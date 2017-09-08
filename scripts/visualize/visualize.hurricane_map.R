b6f1ba0f7ba51454791be6ba2890449c7df9c635
Jordan S Read <jread@usgs.gov>
2017-09-08 15:35:16 -0500

    style PR

--- scripts/visualize/visualize.hurricane_map.R
+++ scripts/visualize/visualize.hurricane_map.R
@@ -4,5 +4,13 @@ visualize.hurricane_map <- function(viz = as.viz('hurricane-map')){
   library(xml2)
   svg <- readDepends(viz)[[1]]
   
+  xml_attr(svg, "id") <- viz[['id']]
+  
+  vb <- strsplit(xml_attr(svg, 'viewBox'),'[ ]')[[1]]
+  
+  xml_add_sibling(xml_children(svg)[[1]], 'rect', .where='before', width=vb[3], height=vb[4], class='ocean-water')
+  xml_add_sibling(xml_children(svg)[[1]], 'desc', .where='before', viz[["alttext"]])
+  xml_add_sibling(xml_children(svg)[[1]], 'title', .where='before', viz[["title"]])
+  
   write_xml(svg, file = viz[['location']])
 }
