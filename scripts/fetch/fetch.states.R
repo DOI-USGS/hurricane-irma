c7f895f864bbba910d3a93183133c26b721f79f3
Jordan S Read <jread@usgs.gov>
2017-09-07 20:02:03 -0500

    fetch within view

--- scripts/fetch/fetch.states.R
+++ scripts/fetch/fetch.states.R
@@ -20,16 +20,15 @@ to_sp <- function(..., proj.string, within = NULL){
   return(map.sp.t)
 }
 
+fetch_map_data <- function(..., viz){
+  view <- readDepends(viz)[['view-limits']]
+  out <- to_sp(..., proj.string = view[['proj.string']], 
+               within = as.sp_box(view$xlim, view$ylim, CRS(view[['proj.string']])))
+  saveRDS(out, viz[['location']])
+}
 
 fetch.states <- function(viz = as.viz('states')){
   fetch_map_data('state', viz = viz)
-}
-
-fetch_map_data <- function(..., viz){
-  view <- readDepends(viz)[['view-limits']]
-  out <- to_sp('state', proj.string = view[['proj.string']], 
-               within = as.sp_box(view$xlim, view$ylim, CRS(view[['proj.string']])))
-  saveRDS(out, viz[['location']])
 }
 
 fetch.counties <- function(viz = as.viz('counties')){
