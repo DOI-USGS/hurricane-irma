4c42cceaed375da60952b8c08828553fe3c49f69
wdwatkins <wwatkins@usgs.gov>
2017-09-08 16:08:12 -0500

    working on sites

--- scripts/fetch/fetch.sites.R
+++ scripts/fetch/fetch.sites.R
@@ -1,21 +1,23 @@
 #fetch NWIS iv data, downsample to hourly
 
-fetch.sites <- function(viz){
-  required <- c("location")
+fetch.sites <- function(viz = as.viz('sites-data')){
+  required <- c("location", "pCode")
   checkRequired(viz, required)
-  
+  pCode <- viz[['pCode']]
   library(dplyr)
   counties <- readDepends(viz)[['counties']] 
   #will this be ok with dependency management?
   start.date <-  as.Date(getContentInfo('precip-data')[['start.date']])
   
   site_sum_all <- data.frame()
-  for(i in states){
+  
+  #max 20 counties at once
+  for(i in ){
     sites <- dataRetrieval::readNWISdata(service = "site",
                           seriesCatalogOutput=TRUE,
-                          parameterCd="00060",
+                          parameterCd=pCode,
                           stateCd = i)
-    sites_sum <- filter(sites, parm_cd == "00060",
+    sites_sum <- filter(sites, parm_cd == pCode,
                         data_type_cd == "uv",
                         !site_tp_cd %in% c("LK", "ES", "GW")) %>% #others?
       mutate(end_date = as.Date(end_date)) %>%
