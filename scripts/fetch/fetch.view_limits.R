671ff9eb34ac98c63030cc5e01f430eb29076cc3
David Blodgett <dblodgett@usgs.gov>
2017-09-08 21:45:49 -0500

    reformat view-limits add bbox as output.

--- scripts/fetch/fetch.view_limits.R
+++ scripts/fetch/fetch.view_limits.R
@@ -1,13 +1,20 @@
 fetch.view_limits <- function(viz = as.viz('view-limits')){
   bbox <- viz[['bbox']]
-  sp.bbox <- as.sp_box(xs = bbox[c(1,3)], ys = bbox[c(2,4)], proj.string = sp::CRS("+proj=longlat +datum=WGS84"))
+  sp.bbox <- as.sp_box(xs = bbox[c(1,3)], ys = bbox[c(2,4)], 
+                       proj.string = sp::CRS("+proj=longlat +datum=WGS84"))
   bbox.transformed <- sp::spTransform(sp.bbox, viz[['proj.string']])
   
   view.limits <- get_sp_lims(bbox.transformed, # it would be nice to just use defaults if these aren't included:
-                             width = viz[['width']], height = viz[['height']], pointsize = viz[['pointsize']])
+                             width = viz[['width']], 
+                             height = viz[['height']], 
+                             pointsize = viz[['pointsize']])
   
-  out <- append(view.limits, list(proj.string = viz[['proj.string']],
-    width = viz[['width']], height = viz[['height']], pointsize = viz[['pointsize']]))
+  out <- append(view.limits, 
+                list(proj.string = viz[['proj.string']],
+                     width = viz[['width']], 
+                     height = viz[['height']], 
+                     pointsize = viz[['pointsize']],
+                     latlonbbox = bbox))
   
   saveRDS(out, viz[['location']])
 }
