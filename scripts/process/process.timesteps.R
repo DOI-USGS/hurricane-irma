cd3589a031fd7239ae6e3190401d2a5440b52e9c
Jordan S Read <jread@usgs.gov>
2017-09-09 18:25:33 -0500

    date only for viz

--- scripts/process/process.timesteps.R
+++ scripts/process/process.timesteps.R
@@ -2,6 +2,6 @@ process.timesteps <- function(viz = as.viz('timesteps')){
   #"classifyBins",'storm-location'
   library(dplyr)
   times <- readDepends(viz)[[1L]] %>% select(DateTime) %>% 
-    unique() %>% .$DateTime %>% as.POSIXct %>% format('%b %d %I:%M %p')
+    unique() %>% .$DateTime %>% as.POSIXct %>% format('%B %d')
   cat(jsonlite::toJSON(list(times=times)), file = viz[['location']])
 }
