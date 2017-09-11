920e55aa8d3ef99a0feb4a5d78fc950bf3a632a4
wdwatkins <wwatkins@usgs.gov>
2017-09-11 08:57:18 -0500

    multiple states for nws_convert

--- scripts/fetch/fetch.nws_conversion.R
+++ scripts/fetch/fetch.nws_conversion.R
@@ -1,17 +1,19 @@
-library(readr)
 
 fetch.nws_conversion <- function(viz = as.viz("nws-conversion")){
   states <- viz[['states']]
   
-  message('to do: code this so we can give it multiple states')
-  stopifnot(length(states) == 1)
-  conversion.url <- sprintf("http://www.nws.noaa.gov/oh/hads/USGS/%s_USGS-HADS_SITES.txt", states)
-  conversion.table <- read_delim(conversion.url,
-                                 delim = "|",skip = 4,col_names = FALSE)
+  conversion.table.all <- data.frame()
+  for(s in states) {
+    conversion.url <- sprintf("http://www.nws.noaa.gov/oh/hads/USGS/%s_USGS-HADS_SITES.txt", s)
+    conversion.table.state <- readr::read_delim(conversion.url,
+                                   delim = "|",skip = 4,col_names = FALSE)
+    conversion.table.all <- dplyr::bind_rows(conversion.table.all, conversion.table.state)
+  }
+  
 
-  names(conversion.table) <- c("NWS","USGS","GOES","NWS HSA","lat","lon","name")
-  conversion.table$USGS <- gsub(" ","", conversion.table$USGS)
+  names(conversion.table.all) <- c("NWS","USGS","GOES","NWS HSA","lat","lon","name")
+  conversion.table.all$USGS <- gsub(" ","", conversion.table.all$USGS)
   
   location <- viz[['location']]
-  saveRDS(conversion.table, file=location)
+  saveRDS(conversion.table.all, file=location)
 }
