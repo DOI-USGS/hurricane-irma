6fba62a3e143a789df4ce68c473ad96f72134778
David Blodgett <dblodgett@usgs.gov>
2017-09-11 14:17:42 -0500

    revert some changes to sciencebase.

--- scripts/fetch/fetchPrecip.R
+++ scripts/fetch/fetchPrecip.R
@@ -6,12 +6,21 @@ fetch.precip <- function(viz = as.viz('precip-data')){
   required <- c("location","start.date", "end.date")
   checkRequired(viz, required)
   
-  getPrecip <- function(sb.url, sb.geom, sb.attribute, startDate, endDate, sb.attribute_values = NULL){
+  getPrecip <- function(counties, startDate, endDate){
     
-    stencil <- geoknife::webgeom(url=sb.url,
-                                 geom = sb.geom,
-                                 attribute = sb.attribute,
-                                 values = sb.attribute_values)
+    counties_fips <- maps::county.fips 
+    
+    split_n_drop <- function(x) strsplit(x, ":")[[1]][1]
+
+    counties_fips$polyname <- as.character(sapply(counties_fips$polyname, split_n_drop))
+    
+    counties_fips <- counties_fips %>%
+      dplyr::filter(polyname %in% counties) %>% 
+      mutate(fips = sprintf('%05d', fips))# fips need 5 digits to join w/ geoknife result
+    
+    stencil <- webgeom(geom = 'derivative:US_Counties',
+                       attribute = 'FIPS',
+                       values = unique(counties_fips$fips))
     
     fabric <- webdata(url = 'https://cida.usgs.gov/thredds/dodsC/stageiv_combined', 
                       variables = "Total_precipitation_surface_1_Hour_Accumulation", 
@@ -33,30 +42,10 @@ fetch.precip <- function(viz = as.viz('precip-data')){
   endDate <- as.POSIXct(paste(viz[["end.date"]],"22:00:00"), tz="America/New_York")
   attr(startDate, 'tzone') <- "UTC"
   
-  sb.url <- viz[["sb.url"]]
-  sb.geom <- viz[["sb.geom"]]
-  sb.attribute <- viz[["sb.attribute"]]
-  
-  counties <- names(readDepends(viz)[['counties']])
-  
-  counties_fips <- maps::county.fips 
+  counties <- readDepends(viz)[['counties']] 
 
-  split_n_drop <- function(x) strsplit(x, ":")[[1]][1]
-  
-  counties_fips$polyname <- as.character(sapply(counties_fips$polyname, split_n_drop))
-
-  counties_fips <- counties_fips %>%
-    dplyr::filter(polyname %in% counties) %>% 
-    mutate(fips = sprintf('%05d', fips))# fips need 5 digits to join w/ geoknife result
-  
-  sb.attribute_values <- unique(counties_fips$fips)
-  
-  precip <- getPrecip(sb.url, sb.geom, sb.attribute, startDate, endDate, sb.attribute_values = sb.attribute_values)
-  
+  precip <- getPrecip(names(counties), startDate, endDate)
   attr(precip$DateTime, 'tzone') <- "America/New_York" #back to eastern
   location <- viz[['location']]
   write.csv(precip, file=location, row.names = FALSE)
 }
-
-
-
