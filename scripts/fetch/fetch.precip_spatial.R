941a54dc4d98836762df98a14e27d50d7a745103
Jordan S Read <jread@usgs.gov>
2017-09-13 11:04:53 -0500

    branch wasn't clean

--- scripts/fetch/fetch.precip_spatial.R
+++ scripts/fetch/fetch.precip_spatial.R
@@ -1,24 +1,25 @@
 fetch.precipSpatial <- function(viz = as.viz('precip-spatial')){
-
+  
   if(!viz[['refetch']]){
     return(NULL)
   }
   
-  checkRequired(viz, "cell_size")
-  
   view <- readDepends(viz)[["view-limits"]]
+  
+  ### constants
+  cell_size <- 12000 # meters -- set to make grid more course or dense.
+  states <- c("florida", "georgia", "alabama", "south carolina") # from maps states id list.
+  ### /constants
+  
+  xlim <- view[["xlim"]]
+  ylim <- view[["ylim"]]
   proj.string <- view[["proj.string"]]
   proj <- sp::CRS(proj.string)
-  storm_area <- sp::spTransform(readDepends(viz)[["storm-area-filter"]],proj)
-  states <- readData("states")
-  cell_size <- viz[["cell_size"]] # meters -- set to make grid more course or dense.
   
-  bbox <- sp::bbox(storm_area)
+  x_range <- (xlim[2] - xlim[1])/cell_size
+  y_range <- (ylim[2] - ylim[1])/cell_size
   
-  x_range <- (bbox[3] - bbox[1])/cell_size
-  y_range <- (bbox[4] - bbox[2])/cell_size
-  
-  grid_topology <- sp::GridTopology(c(bbox[1],bbox[2]),
+  grid_topology <- sp::GridTopology(c(xlim[1],ylim[1]),
                                     cellsize = c(cell_size,cell_size),
                                     cells.dim = c(x_range, y_range))
   sp_grid <- raster::raster(sp::SpatialGrid(grid_topology, proj))
@@ -26,42 +27,36 @@ fetch.precipSpatial <- function(viz = as.viz('precip-spatial')){
   sp_cells <- raster::rasterToPolygons(sp_grid)
   sp_points <- sp::SpatialPoints(raster::rasterToPoints(sp_grid),proj)
   
-  IDs <- sapply(slot(sp_cells, "polygons"), function(x) slot(x, "ID"))
-  sp_cells <- sp::SpatialPolygonsDataFrame(sp_cells, data.frame(id=c(1:length(sp_cells)), row.names = IDs))
-  sp_point_ids <- sp::over(sp_points, sp_cells, returnList = F, fn=NULL)
-  sp_points <- sp::SpatialPointsDataFrame(sp_points, sp_point_ids)
-
-  stateIDs <- sapply(slot(states, "polygons"), function(x) slot(x, "ID"))
-  state_boundary <- rgeos::gBuffer(sp::SpatialPolygonsDataFrame(states, 
-                                             data.frame(viz=rep("viz", 
-                                                                length(stateIDs)), 
-                                                        stringsAsFactors = F, 
-                                                        row.names = stateIDs)),
-                                   byid = T,
-                                   width = 0) # zero buffer to fix topology exception.
+  state_boundary <- to_sp("state", proj.string = proj.string)
   
+  state_IDs <- sapply(slot(state_boundary, "polygons"), function(x) slot(x, "ID"))
+  index <- which(state_IDs %in% states)
+  
+  state_boundary <- SpatialPolygonsDataFrame(state_boundary[index,], data.frame(viz=rep("viz", length(index)), 
+                                                                                stringsAsFactors = F, row.names = states))
   state_boundary <- maptools::unionSpatialPolygons(state_boundary, state_boundary@data$viz)
   
-  storm_states <- rgeos::gIntersection(storm_area, state_boundary)
+  sp_cells <- rgeos::gIntersection(state_boundary, sp_cells, byid = T, drop_lower_td = T)
   
-  sp_cells <- sp_cells[which(!is.na(sp::over(sp_cells, storm_states, returnList = F))),]
+  IDs <- sapply(slot(sp_cells, "polygons"), function(x) slot(x, "ID"))
+  sp_cells <- SpatialPolygonsDataFrame(sp_cells, data.frame(id=c(1:length(sp_cells)), row.names = IDs))
   
-  sp_cells <- rgeos::gIntersection(sp_cells, state_boundary, byid = T)
+  sp_point_selector <- sp::over(sp_points, sp_cells, fn=NULL)
   
-  IDs <- sapply(slot(sp_cells, "polygons"), function(x) strsplit(slot(x, "ID")," ")[[1]][1])
+  sp_points <- sp_points[which(!is.na(sp_point_selector))]
   
-  sp::spChFIDs(sp_cells) <- IDs
+  sp_point_ids <- sp::over(sp_points, sp_cells, returnList = F, fn=NULL)
   
-  sp_cells <- sp::SpatialPolygonsDataFrame(sp_cells, data.frame(id = as.numeric(IDs), row.names = IDs))
-  
-  sp_points <- sp_points[which(sp_points@data$id %in% sp_cells@data$id),]
+  sp_points <- SpatialPointsDataFrame(sp_points, sp_point_ids)
   
   saveRDS(sp_cells, viz[['location']])
   
-  rgdal::writeOGR(sp_cells, "irma_cells.shp",
-                  layer = "irma_cells", driver = "ESRI Shapefile", overwrite_layer=TRUE) 
+  # Used for ScienceBase item.
+  # rgdal::writeOGR(sp_points, irma_points.shp, # Will be used later instead of cells.
+  #                 layer = "irma_points", driver = "ESRI Shapefile", overwrite_layer=TRUE)
+  # rgdal::writeOGR(sp_cells, "irma_cells.shp",
+  #                 layer = "irma_cells", driver = "ESRI Shapefile", overwrite_layer=TRUE)
 }
-
 
 
 
